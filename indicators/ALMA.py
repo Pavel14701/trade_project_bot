817c8006aa4d0a5dcdb4b395d94cffdc902d6a6c
Pavel14701 <pavelkutia@gmail.com>
2024-05-29 20:46:12 +0300

    commit

--- indicators/ALMA.py
+++ indicators/ALMA.py
@@ -3,24 +3,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
-
+instId - 'sdasdsada'
 class Alma:
-    def __init__(self, lenghts=None, instId, timeframe, test=None):
+    def __init__(self, data = None, lenghts=None, instId = None, timeframe = None, test=None):
         self.lenghts = lenghts
         self.instId = instId
         self.timeframe = timeframe
         self.test = test
+        self.test = data
         
-    def load_dest_data(ticker, start, end, timeframe):
-        if self.test not None:
+    def load_dest_data(self, ticker, start, end, timeframe):
+        if self.test == True:
             # Загружаем данные по акциям Apple
             data = yf.download(ticker, start, end, interval=timeframe)
             return (test_data, ticker)
         else:
             pass
         
-    def calculate(self):
-        if self.test not None:
+    def calculate(self, test_data=None):
+        if self.test == True:
+            data = test_data
         # Вычисляем ALMA с длиной окна 20 и стандартными параметрами
         lenghts = self.lenghts
         m = (N - 1) / 2
@@ -31,7 +33,7 @@ class Alma:
         return alma_data
 
     def create_vizualization(self, alma_data, ticker=None):
-        if self.test not None:
+        if self.test == True:
             inst = ticker
         else:
             inst = self.instId
