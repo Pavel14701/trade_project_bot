4aea3c00ec365ee6048714bd5091059a8fb3dcaa
Pavel14701 <pavelkutia@gmail.com>
2024-07-04 10:12:17 +0300

    commit

--- broker.py
+++ broker.py
@@ -10,10 +10,16 @@ listner_instance2 = OKXIventListner(settings['instId'][1])
 
 def listner1(listner_instance1):
     listner_instance1.create_listner()
+    
+thread1 = Thread(target=listner1)
+
 
 def listner2(listner_instance2):
     listner_instance2.create_listner()
 
+thread2 = Thread(target=listner2)
+
 
 if __name__ == '__main__':
-    
+    thread1.start()
+    thread2.start()
