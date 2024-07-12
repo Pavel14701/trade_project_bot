a38549108b7889d8d513a93e0f119a2097f55fb3
Pavel14701 <pavelkutia@gmail.com>
2024-07-12 17:34:59 +0300

    commit

--- sim.py
+++ sim.py
@@ -5,9 +5,9 @@ from datasets.database import DataAllDatasets
 
 bd = DataAllDatasets('BTC-USDT-SWAP', '4H')
 get = UserInfo('BTC-USDT-SWAP', '4H')
-timestamps = []
-for timestamp in timestamps:
-    result = get.get_market_data(load_data_after=timestamp)
+dates = [] 
+for date in dates:
+    result = get.get_market_data(load_data_before=date, lengths=300)
     bd.add_data_to_db(result)
 data = bd.get_all_bd_marketdata()
 df = pd.DataFrame(data)
