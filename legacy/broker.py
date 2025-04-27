9658b91520e53444bdf03c89e83d5ba25aba602a
Pavel14701 <pavelkutia@gmail.com>
2025-04-28 01:18:13 +0300

    Removed legacy from Git tracking

--- legacy/broker.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from configs.load_settings import ConfigsProvider
-from listners.ivent_listner import OKXIventListner
-
-
-if __name__ == '__main__':
-    settings = ConfigsProvider().load_user_configs()
-    listner_instance = OKXIventListner()
-    listner_instance.create_listner()
