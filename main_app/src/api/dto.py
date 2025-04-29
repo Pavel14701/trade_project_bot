7a9d9815d73af345eca6da69dcfbaf74ebd6bd1d
Pavel14701 <pavelkutia@gmail.com>
2025-04-29 05:22:15 +0300

    commit

--- main_app/src/api/dto.py
+++ main_app/src/api/dto.py
@@ -1,18 +1,126 @@
 from dataclasses import dataclass
-from datetime import datetime
 
 
 @dataclass(slots=True, frozen=True)
-class PriceDataRequestDTO:
-    inst_id: str
-    _from: datetime | None = None
-    to: datetime | None = None
+class GetPriceDataDTO:
+    instId: str
+    bar: str | None = None
+    after: str | None = None
+    before: str | None = None
+    limit: str | None = None
+
+
+@dataclass(slots=True, frozen=True)
+class GetMarkPriceDTO:
+    instType: str
+    instId: str | None = None
+
+
+@dataclass(slots=True, frozen=True)
+class GetBalanceDTO:
+    instType: str | None = None
+    instId: str | None = None
+    posId: str | None = None
+
+
+@dataclass(slots=True, frozen=True)
+class SetLeverageDTO:
+    instId: str
+    ccy: str
+    lever: str
+    mgnMode: str
+    posSide: str
+
+
+@dataclass(slots=True, frozen=True)
+class GetLeverageDTO:
+    instId: str
+    ccy: str
+    mgn_mode: str
+
+
+@dataclass(slots=True, frozen=True)
+class PlaceOrderDTO:
+    instId: str
+    tdMode: str
+    side: str
+    orderType: str
+    sz: str
+    px: str
+    ccy: str | None = None
+    clOrdId: str | None = None
+    tag: str | None = None
+    posSide: str | None = None
+    pxUsd: str | None = None
+    pxVol: str | None = None
+    reduceOnly: str | None = None
+    tgtCcy: str | None = None
+    banAmend: bool | None = None
+    stpMode: str | None = None
+    attachAlgoOrds: list["AttachAlgoOrds"] | None = None
+
+
+@dataclass(slots=True, frozen=True)
+class AmendOrderDTO:
+    instId: str
+    cxlOnFail: bool | None = None
+    ordId: str | None = None
+    clOrdId: str | None = None
+    reqId: str | None = None
+    newSz: str | None = None
+    newPx: str | None = None
+    newPxUsd: str | None = None
+    newPxVol: str | None = None
+    attachAlgoOrds: list["AttachAlgoOrds"]
+
+
+@dataclass(slots=True, frozen=True)
+class AttachAlgoOrds:
+    attachAlgoClOrdId: str | None = None 
+    tpTriggerPx: str | None = None
+    tpOrdPx: str | None = None
+    tpOrdKind: str | None = None
+    slTriggerPx: str | None = None
+    slOrdPx: str | None = None
+    tpTriggerPxType: str | None = None
+    slTriggerPxType: str | None = None
+    sz: str| None = None
+    amendPxOnTriggerType: str | None = None
+
+
+@dataclass(slots=True, frozen=True)
+class CancelOrerDTO:
+    instId: str
+    ordId: str | None = None
+    clOrdId: str | None = None
+
+
+@dataclass(frozen=True, slots=True)
+class ClosePositionsDTO:
+    instId: str
+    posSide: str | None = None
+    mgnMode: str
+    ccy: str | None = None
+    autoCxl: bool | None = None
+    clOrdId: str | None = None
+    tag: str | None = None
+
+
+@dataclass(frozen=True, slots=True)
+class GetOrderDetailsDTO:
+    instId: str
+    ordId: str | None = None
+    clOrdId: str | None = None
+
+
+@dataclass(frozen=True, slots=True)
+class GetOrderListDTO:
+    instType: str | None = None
+    uly: str | None = None
+    instFamily: str | None = None
+    instId: str | None = None
+    ordType: str | None = None
+    state: str | None = None
+    after: str | None = None
+    before: str | None = None
     limit: int | None = None
-    timeframe: str
-    is_demo: bool
-
-
-@dataclass(slots=True, frozen=True)
-class MarkPriceRequestDto:
-    inst_type: str
-    inst_id: str | None = None
--- main_app/src/api/interfaces.py
+++ main_app/src/api/interfaces.py
@@ -1,32 +1,95 @@
 from typing import Any, Protocol
 
 from main_app.src.api.dto import (
-    MarkPriceRequestDto, 
-    PriceDataRequestDTO
+    AmendOrderDTO,
+    CancelOrerDTO,
+    GetBalanceDTO,
+    GetLeverageDTO,
+    GetMarkPriceDTO,
+    GetOrderDetailsDTO,
+    GetOrderListDTO, 
+    GetPriceDataDTO,
+    PlaceOrderDTO,
+    SetLeverageDTO,
+    ClosePositionsDTO
 )
 
+
 class IOkxTrade(Protocol):
-    pass
+    # https://www.okx.com/docs-v5/en/#overview-rest-authentication-signature
+    # https://www.okx.com/docs-v5/en/#overview-rest-authentication-making-requests
+
+    async def get_account_balance(self, ccy: str) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#trading-account-rest-api-get-balance
+        ...
+
+    async def get_positions(self, params: GetBalanceDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#trading-account-rest-api-get-positions
+        ...
+
+    async def set_position_mode(self, posMode: str) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#trading-account-rest-api-set-position-mode
+        ...
+
+    async def set_leverage(self, params: SetLeverageDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#trading-account-rest-api-set-leverage
+        ...
+
+    async def get_leverage(self, params: GetLeverageDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#trading-account-rest-api-get-leverage
+        ...
+
+    async def place_order(self, params: PlaceOrderDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#order-book-trading-trade-post-place-order
+        ...
+
+    async def amend_order(self, params: AmendOrderDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/?shell#order-book-trading-trade-post-amend-order
+        ...
+
+    async def cancel_order(self, params: CancelOrerDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/?shell#order-book-trading-trade-post-cancel-order
+        ...
+
+    async def close_positions(self, params: ClosePositionsDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/?shell#order-book-trading-trade-post-close-positions
+        ...
+
+    async def get_order_details(self, params: GetOrderDetailsDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/?shell#order-book-trading-trade-get-order-details
+        ...
+
+    async def get_order_list(self, params: GetOrderListDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/?shell#order-book-trading-trade-get-order-list
+        ...
+
+
 
 class IOkxMarketData(Protocol):
-    async def get_candlesticks(self, params: PriceDataRequestDTO) -> dict[str, Any]: 
-    # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-candlesticks
+    async def get_candlesticks(self, params: GetPriceDataDTO) -> dict[str, Any]: 
+        # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-candlesticks
         ...
 
-    async def get_candlesticks_history(self, params: PriceDataRequestDTO) -> dict[str, Any]:
-    # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-candlesticks-history
+    async def get_candlesticks_history(self, params: GetPriceDataDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-candlesticks-history
         ...
 
-    async def get_ticker(self, ) -> dict[str, Any]:
+    async def get_ticker(self, instId: str) -> dict[str, Any]:
         # https://www.okx.com/docs-v5/en/#order-book-trading-market-data-get-ticker
         ...
 
-    async def get_mark_price(self, params: MarkPriceRequestDto) -> dict[str, str]:
-    # https://www.okx.com/docs-v5/en/#public-data-rest-api-get-mark-price
+    async def get_mark_price(self, params: GetMarkPriceDTO) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#public-data-rest-api-get-mark-price
         ...
 
-    async def get_instruments(self, inst_id: str) -> dict[str, Any]:
-    # https://www.okx.com/docs-v5/en/#public-data-rest-api-get-instruments
+    async def get_instruments(self, instId: str) -> dict[str, Any]:
+        # https://www.okx.com/docs-v5/en/#public-data-rest-api-get-instruments
+        ...
+
+
+class IOkxExcHandler(Protocol):
+    async def handle_exception(self):
+        # https://www.okx.com/docs-v5/en/#error-code
         ...
 
 
